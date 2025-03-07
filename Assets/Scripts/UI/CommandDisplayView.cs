7386536d45bb09ded249cbe73ca46486dc2d2c0a
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-07 18:34:09 +0900

    Update CommandDisplayView.cs
    현재의 커맨드 불러오기

--- Assets/Scripts/UI/CommandDisplayView.cs
+++ Assets/Scripts/UI/CommandDisplayView.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using Ingame;
 using Ingame.Player;
 using UnityEngine;
@@ -13,12 +14,17 @@ namespace UI
         [SerializeField] private Color[] commandColors = new Color[2];
         [SerializeField] private Image commandPrefab;
 
+        private CommandManager.Command[] _commands;
         private Image[] _commandObjects;
         private int _count;
 
         private void Start()
         {
-            _commandObjects = new Image[StageInfo.CommandCount];
+            var commandCount = StageInfo.CommandCount;
+            
+            _commands = new CommandManager.Command[commandCount];
+            
+            _commandObjects = new Image[commandCount];
             for (var i = 0; i < _commandObjects.Length; i++)
             {
                 var image = Instantiate(commandPrefab, transform);
@@ -33,10 +39,22 @@ namespace UI
         {
             if (_count >= _commandObjects.Length) return;
             
+            _commands[_count] = command;
             _commandObjects[_count].enabled = true;
             _commandObjects[_count].color = commandColors[(int)command];
+            
+            Debug.Log(GetCommand());
 
             _count++;
+        }
+
+        public string GetCommand()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i <= _count; i++)
+                sb.Append(((int)_commands[i]).ToString());
+
+            return sb.ToString();
         }
 
         public void ClearDisplay()
