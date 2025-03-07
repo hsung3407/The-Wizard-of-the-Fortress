1ccd4d4a6a59c6064d6ae01a49e6096778bc5b20
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-07 16:51:01 +0900

    Update PlayerControl.cs
    마법 사용 상호작용 구역 제한

--- Assets/Scripts/Ingame/Player/PlayerControl.cs
+++ Assets/Scripts/Ingame/Player/PlayerControl.cs
@@ -13,47 +13,35 @@ public class PlayerControl : MonoBehaviour
 
     [SerializeField] private Camera ingameCamera;
     [SerializeField] private RectTransform ingameViewRect;
+    [SerializeField] private RectTransform touchAreaRect;
     private readonly Vector3[] _ingameViewCorners = new Vector3[4];
+    private readonly Vector3[] _touchAreaCorners = new Vector3[4];
 
     [SerializeField] private PredictorManager predictorManager;
 
     private readonly RaycastHit[] _hits = new RaycastHit[1];
     LayerMask _layerMask;
-    
+
     private void Awake()
     {
         _layerMask = LayerMask.GetMask("Ground", "Default");
-        
-        pointInput.action.performed += c =>
-        {
-            if (!_predicating) return;
-            
-            var ray = IngameViewToRay(pointInput.action.ReadValue<Vector2>());
-            if (Physics.RaycastNonAlloc(ray, _hits, 30, _layerMask) > 0)
-            {
-                if (_hits[0].transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
-                {
-                    predictorManager.PosUpdate(_hits[0].point);
-                    return;
-                }
-            }
-            
-            predictorManager.SetPredictor(PredictorManager.PredictorType.None);
-            _predicating = false;
-        };
+
 
         pressInput.action.performed += _ =>
         {
-            var ray = IngameViewToRay(pointInput.action.ReadValue<Vector2>());
+            var point = pointInput.action.ReadValue<Vector2>();
+            if (!IsInTouchArea(point)) return;
+
+            var ray = IngameViewToRay(point);
             if (Physics.RaycastNonAlloc(ray, _hits, 30, _layerMask) < 1) return;
             if (_hits[0].transform.gameObject.layer != LayerMask.NameToLayer("Ground")) return;
             _predicating = true;
 
-             //현재는 테스트용
-             //TODO 마법 정보 불러와서 전달하기
-             predictorManager.SetPredictor(PredictorManager.PredictorType.Square);
-             predictorManager.PosUpdate(_hits[0].point);
-       };
+            //현재는 테스트용
+            //TODO 마법 정보 불러와서 전달하기
+            predictorManager.SetPredictor(PredictorManager.PredictorType.Square);
+            predictorManager.PosUpdate(_hits[0].point);
+        };
 
         pressInput.action.canceled += _ =>
         {
@@ -62,11 +50,41 @@ public class PlayerControl : MonoBehaviour
             predictorManager.SetPredictor(PredictorManager.PredictorType.None);
             Fire();
         };
+        
+        pointInput.action.performed += c =>
+        {
+            Debug.Log(Time.deltaTime);
+            var point = c.ReadValue<Vector2>();
+            if (!_predicating) return;
+
+            if (IsInTouchArea(point))
+            {
+                var ray = IngameViewToRay(point);
+                if (Physics.RaycastNonAlloc(ray, _hits, 30, _layerMask) > 0)
+                {
+                    if (_hits[0].transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                    {
+                        predictorManager.PosUpdate(_hits[0].point);
+                        return;
+                    }
+                }
+            }
+
+            predictorManager.SetPredictor(PredictorManager.PredictorType.None);
+            _predicating = false;
+        };
     }
 
     private void Start()
     {
         ingameViewRect.GetWorldCorners(_ingameViewCorners);
+        touchAreaRect.GetWorldCorners(_touchAreaCorners);
+    }
+
+    private bool IsInTouchArea(Vector3 origin)
+    {
+        return origin.x > _touchAreaCorners[0].x && origin.y > _touchAreaCorners[0].y &&
+               origin.x < _touchAreaCorners[2].x && origin.y < _touchAreaCorners[2].y;
     }
 
     private Ray IngameViewToRay(Vector3 origin)
@@ -81,5 +99,4 @@ public class PlayerControl : MonoBehaviour
     private void Fire()
     {
     }
-    
 }
