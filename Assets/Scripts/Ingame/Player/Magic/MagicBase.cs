77c4d7871916f4b48789fbe25115248643db0116
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-12 23:21:16 +0900

    revert
    복구

--- Assets/Scripts/Ingame/Player/Magic/MagicBase.cs
+++ Assets/Scripts/Ingame/Player/Magic/MagicBase.cs
@@ -1,12 +1,10 @@
+using Ingame.Player.Predictor;
 using UnityEngine;
 
 namespace Ingame.Player
 {
     public abstract class MagicBase : MonoBehaviour
     {
-        public void DrawPredictionArea(Vector3 position)
-        {
-            
-        }
+        public PredictorManager.PredictorType predictorType;
     }
 }
