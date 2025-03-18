d81c359022f146cad8b22eed8037856c2812559a
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-18 19:00:27 +0900

    refactoring: MagicDataSO

---
+++ Assets/Scripts/SO/MagicDataSO.cs
@@ -0,0 +1,28 @@
+using Ingame.Player.Predictor;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Ingame.Player
+{
+    [CreateAssetMenu(fileName = "MagicDataSO", menuName = "Scriptable Objects/MagicDataSO")]
+    public class MagicDataSO : ScriptableObject
+    {
+        [Header("Identify")]
+        [SerializeField] private string magicName;
+        [SerializeField] private string command;
+        
+        [Header("Description")]
+        [SerializeField] private Sprite icon;
+        [SerializeField] private string description;
+
+        [Header("Data")] 
+        [SerializeField] private PredictorManager.PredictorType predictorType;
+        [SerializeField] private MagicBase magicObject;
+    
+        public string MagicName => magicName;
+        public string Command => command;
+        
+        public PredictorManager.PredictorType PredictorType => predictorType;
+        public MagicBase MagicObject => magicObject;
+    }
+}
