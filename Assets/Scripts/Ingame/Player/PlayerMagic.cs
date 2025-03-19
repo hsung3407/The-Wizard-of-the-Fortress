310ada5546ff42e44889422d6b11acd5a08dd381
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-19 10:47:10 +0900

    Update PlayerMagic.cs

--- Assets/Scripts/Ingame/Player/PlayerMagic.cs
+++ Assets/Scripts/Ingame/Player/PlayerMagic.cs
@@ -1,9 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Ingame.Player
 {
     public class PlayerMagic : MonoBehaviour
     {
-        
+        private Dictionary<string, MagicDataSO> _magicDataDict = new Dictionary<string, MagicDataSO>();
+
+        private void Start()
+        {
+            var data = Resources.LoadAll<MagicDataSO>("MagicDataSO");
+            _magicDataDict = data.ToDictionary(datum=>datum.Command, datum=>datum);
+        }
+
+        public bool GetMagicDataWithCommand(string command, out MagicDataSO data)
+        {
+            var check =  _magicDataDict.TryGetValue(command, out data);
+            if (!check)
+            {
+                Debug.Log($"해당 커맨드를 가진 정보가 없음 : {command}");
+            }
+            return check;
+        }
     }
 }
