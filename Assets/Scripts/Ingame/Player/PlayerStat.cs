3886d9ed731f3b3dca574c939a8b338f89e3fa4c
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-24 20:12:02 +0900

    Update PlayerStat.cs
    마나 재생 추가

--- Assets/Scripts/Ingame/Player/PlayerStat.cs
+++ Assets/Scripts/Ingame/Player/PlayerStat.cs
@@ -7,11 +7,17 @@ namespace Ingame.Player
     public class PlayerStat : MonoBehaviour
     {
         [SerializeField] private float maxMana = 100;
+        [SerializeField] private float manaRegenerationAmountPerSec = 10;
         private float _mana;
 
         private void Start()
         {
             _mana = maxMana;
+        }
+
+        private void Update()
+        {
+            _mana = Mathf.Clamp(_mana + manaRegenerationAmountPerSec * Time.deltaTime, 0, maxMana);
         }
 
         public bool UseMana(float mana)
