15e16e766d29a8b564254b39580f3f5238170300
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-25 23:15:10 +0900

    refactoring: renaming stage system

--- Assets/Scripts/Ingame/EnemySpawner.cs
+++ Assets/Scripts/Ingame/EnemySpawner.cs
@@ -1,23 +1,24 @@
 using System;
 using System.Collections;
+using SO;
 using UnityEngine;
 using Utility;
 using Random = UnityEngine.Random;
 
 namespace Ingame
 {
-    public class MobGenerator : MonoBehaviour
+    public class EnemySpawner : MonoBehaviour
     {
         private Collider _spawnArea;
         private Transform _castleWallTr;
-
+        
         private void Awake()
         {
             _spawnArea = GetComponent<Collider>();
             _castleWallTr = GameObject.FindWithTag("CastleWall").transform;
         }
-
-        public void SpawnMob()
+        
+        public void SpawnEnemy()
         {
             var go = ObjectPool.Instance.Get(StageInfo.MobType);
             go.transform.position = new Vector3(Random.Range(_spawnArea.bounds.min.x, _spawnArea.bounds.max.x),
---
+++ Assets/Scripts/Ingame/IngameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using SO;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Ingame
+{
+    public class IngameManager : MonoBehaviour
+    {
+        [SerializeField] private float waveDelay = 5;
+        private StageWaveDataSO[] _waveData;
+        private int _waveIndex;
+        
+        [SerializeField] private EnemySpawner enemySpawner;
+
+        private void Awake()
+        {
+            _waveData = Resources.LoadAll<StageWaveDataSO>($"Stage/{StageInfo.stageIndex}");
+            Debug.Log($"{_waveData.Length}");
+        }
+
+        private void Start()
+        {
+            _waveIndex = 0;
+            StartCoroutine(MainFlow());
+        }
+
+        private IEnumerator MainFlow()
+        {
+            //TODO: Wave Start Notify Display
+            int waveDataCount = _waveData.Length;
+            for (_waveIndex = 0; _waveIndex < waveDataCount; _waveIndex++)
+            {
+                Debug.Log($"Wave Start : {_waveIndex}");
+            
+                var data = _waveData[_waveIndex];
+                var spawnDelay = new WaitForSeconds(1 / data.GeneratePerSec);
+                int enemyCount = data.EnemyCount;
+                for (int i = 0; i < enemyCount; i++)
+                {
+                    enemySpawner.SpawnEnemy();
+                    yield return spawnDelay;
+                }
+
+                Debug.Log($"Wave End : {_waveIndex}");
+            
+                yield return new WaitForSeconds(waveDelay);
+            }
+        
+            Debug.Log($"Stage Clear");
+        }
+    }
+}
