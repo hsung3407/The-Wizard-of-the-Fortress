7c39240fcfa5c3cf070cd2e0ec5e7c4b1dfc91e4
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-27 19:27:08 +0900

    add: main stage select system

---
+++ Assets/Scripts/Main/StageManager.cs
@@ -0,0 +1,41 @@
+using Ingame;
+using SO;
+using UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Main
+{
+    public class StageManager : MonoBehaviour
+    {
+        [SerializeField] private StageInfoView stageInfoView;
+    
+        private void Awake()
+        {
+            var stageData = Resources.LoadAll<StageDataSO>("StageData");
+        
+            var buttons = GetComponentsInChildren<Button>();
+
+            if(stageData.Length != buttons.Length) Debug.LogError("스테이지 데이터 & 버튼 개수 불일치");
+        
+            for (var i = 0; i < stageData.Length; i++)
+            {
+                var button = buttons[i];
+                var data = stageData[i];
+                button.onClick.RemoveAllListeners();
+                button.onClick.AddListener(() =>
+                {
+                    StaticStageInfo.StageIndex = data.StageIndex;
+                    StaticStageInfo.CommandCount = data.CommandCount;
+                    stageInfoView.DisplayView(data);
+                });
+            }
+        }
+
+        public void StartStage()
+        {
+            SceneManager.LoadScene("Ingame");
+        }
+    }
+}
