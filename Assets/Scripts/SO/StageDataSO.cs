21aeb3e8c2a108c3b900745da7aa32e8613533a9
hsung3407 <azxccxzavv3407@gmail.com>
2025-03-27 19:23:19 +0900

    updata: stage data SO

---
+++ Assets/Scripts/SO/StageDataSO.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace SO
+{
+    [CreateAssetMenu(fileName = "StageDataSO", menuName = "Scriptable Objects/StageDataSO")]
+    public class StageDataSO : ScriptableObject
+    {
+        [SerializeField] private string stageName;
+        [SerializeField] private int stageIndex;
+        [SerializeField] private int commandCount;
+        
+        public string StageName => stageName;
+        public int StageIndex => stageIndex;
+        public int CommandCount => commandCount;
+    }
+}
