7fc487afe3b8bdfdcae00e73da8e37e9cbc77660
hsung3407 <azxccxzavv3407@gmail.com>
2025-04-02 11:58:22 +0900

    refactoring: enemy

--- Assets/Scripts/Ingame/Enemy.cs
+++ Assets/Scripts/Ingame/Enemy.cs
@@ -4,17 +4,32 @@ using UnityEngine.Serialization;
 
 namespace Ingame
 {
+    [Serializable]
+    public class EnemyBaseStat
+    {
+        [SerializeField] private float maxHealth = 100;
+        [SerializeField] private float damage = 1;
+        [SerializeField] private float speed = 1;
+        [SerializeField] private float speedMultiplier = 1;
+        
+        public float MaxHealth => maxHealth;
+        public float Damage => damage;
+        public float Speed => speed;
+        public float SpeedMultiplier => speedMultiplier;
+        
+    }
+    
     public class Enemy : MonoBehaviour
     {
-        [SerializeField] private float maxHealth = 100f;
+        [SerializeField] private EnemyBaseStat baseStat;
+        
         private float _health;
-
-        [SerializeField] private float damage = 1f;
-        public float Damage => damage;
-
-        [SerializeField] private float speed = 1;
+        private float _damage = 1f;
+        private float _speed = 1;
         private float _speedMultiplier = 1;
 
+        public float Damage => _damage;
+        
         private event Action OnDie;
         private bool isDie;
 
@@ -31,13 +46,16 @@ namespace Ingame
 
         private void Update()
         {
-            transform.position += transform.forward * Mathf.Max(speed * _speedMultiplier * Time.deltaTime, 0);
+            transform.position += transform.forward * Mathf.Max(_speed * _speedMultiplier * Time.deltaTime, 0);
         }
 
         private void Init()
         {
             isDie = false;
-            _health = maxHealth;
+            _health = baseStat.MaxHealth;
+            _damage = baseStat.Damage;
+            _speed = baseStat.Speed;
+            _speedMultiplier = baseStat.SpeedMultiplier;
         }
 
         public void Init(Action onDie)
@@ -51,7 +69,7 @@ namespace Ingame
             switch (statType)
             {
                 case StatType.Speed:
-                    speed += amount;
+                    _speed += amount;
                     break;
                 case StatType.SpeedMultiplier:
                     _speedMultiplier += amount;
