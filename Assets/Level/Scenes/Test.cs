30807e6248c32401daaf7fc9b6b83ac1bfe42164
hsung3407 <azxccxzavv3407@gmail.com>
2025-04-02 11:23:57 +0900

    test

--- Assets/Level/Scenes/Test.cs
+++ Assets/Level/Scenes/Test.cs
@@ -5,51 +5,13 @@
 
 public class Test : MonoBehaviour
 {
-    private void Awake()
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Awake");
+        Debug.Log("Trigger Enter");
     }
 
-    void OnEnable()
+    private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Enabled");
-    }
-    
-    void Start()
-    {
-        Debug.Log("Start");
-    }
-
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            var gameobject = new GameObject
-            {
-                name = "Test Gen"
-            };
-
-            var type = Type.GetType("Test");
-            var t = gameobject.AddComponent(type);   
-        }
-
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            Debug.Log($"Invoke On Test : {Time.time} / {Time.frameCount} - {Time.deltaTime}");
-            BroadcastMessage("OnTest");
-            StartCoroutine(TestTime());
-        }
-    }
-
-    protected void OnTest()
-    {
-        Debug.Log($"OnTest : {Time.time}");
-    }
-
-    IEnumerator TestTime()
-    {
-        yield return null;
-        Debug.Log($"TestTime : : {Time.time} / {Time.frameCount} - {Time.deltaTime}");
+        Debug.Log("Trigger Exit");
     }
 }
