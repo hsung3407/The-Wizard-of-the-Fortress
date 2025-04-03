0488c8fbfb0efc2ec6231e20144dbdbd09aeabab
hsung3407 <azxccxzavv3407@gmail.com>
2025-04-03 20:28:43 +0900

    refactoring: sphere tick detector

---
+++ Assets/Scripts/Ingame/Player/Magic/Detector/SphereTickDetector.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Ingame.Player.Magic.Detector
+{
+    public class SphereTickDetector : DetectorBase
+    {
+        [FormerlySerializedAs("range")] [SerializeField] private float radius = 1;
+        [SerializeField] private float tickDelay = 1;
+
+        private void Start()
+        {
+            StartCoroutine(Tick());
+        }
+
+        private IEnumerator Tick()
+        {
+            var delay = new WaitForSeconds(tickDelay);
+
+            while (true)
+            {
+                foreach (var detectedCollider in Physics.OverlapSphere(transform.position,
+                             radius,
+                             LayerMask.GetMask("Enemy")))
+                {
+                    OnDetect(detectedCollider.GetComponent<Enemy>());
+                }
+
+                yield return delay;
+            }
+        }
+    }
+}
