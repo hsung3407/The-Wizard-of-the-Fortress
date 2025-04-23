80861b9b8786923e1a1ff21965a91dca915bc782
hsung3407 <azxccxzavv3407@gmail.com>
2025-04-23 21:38:48 +0900

    Update LinkedListExtension.cs

--- Assets/Scripts/Utility/LinkedListExtension.cs
+++ Assets/Scripts/Utility/LinkedListExtension.cs
@@ -4,8 +4,10 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Utility
 {
-    public static class DataStructureExpand
+    public static class LinkedListExtension
     {
+        extension
+        
         public static IEnumerable<LinkedListNode<T>> GetNodes<T>(this LinkedList<T> list)
         {
             for (var node = list.First; node != null; node = node.Next) yield return node;
