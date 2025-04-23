9a1c914324b6b0254c8b698ec7c272f8d2832e48
hsung3407 <azxccxzavv3407@gmail.com>
2025-04-23 09:45:47 +0900

    Update Test.cs
    null test

--- Assets/Level/Scenes/Test.cs
+++ Assets/Level/Scenes/Test.cs
@@ -6,27 +6,70 @@ using Ingame;
 using Ingame.Player;
 using UnityEngine;
 
-public class TsetClass
-{
-    public void TestLog()
-    {
-        Debug.Log("TestLog");
-    }
-}
-
-public class TestTT : TsetClass
-{
-    public new void TestLog()
-    {
-        Debug.Log("TestLog Two");
-    }
-}
-
 public class Test : MonoBehaviour
 {
+    Dictionary<object, int> map = new Dictionary<object, int>();
+    List<object> list = new List<object>();
+
+    private int i;
     private void Awake()
     {
-        var c = new TestTT();
-        c.TestLog();
+        for (i = 0; i < 10; i++)
+        {
+            var newObj = new GameObject
+            {
+                name = "Test" + i
+            };
+            list.Add(newObj);
+            map.Add(newObj, i);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            var newObj = new GameObject
+            {
+                name = "Test" + i
+            };
+            list.Add(newObj);
+            map.Add(newObj, i);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            foreach (GameObject o in list)
+            {
+                Destroy(o);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("List");
+            foreach (var obj in list)
+            {
+                Debug.Log(obj);
+            }
+            Debug.Log("Map");
+            foreach (var keyValuePair in map)
+            {
+                Debug.Log(keyValuePair);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            foreach (var keyValuePair in map)
+            {
+                Debug.Log(map[null!]);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            map.Remove(null!);
+        }
     }
 }
