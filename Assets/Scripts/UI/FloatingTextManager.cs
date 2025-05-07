6f21a94bcb351d3511ba6be691fd4994d1d6deea
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-07 15:25:11 +0900

    update: floating message
    오브젝트 풀링 적용

--- Assets/Scripts/UI/FloatingTextManager.cs
+++ Assets/Scripts/UI/FloatingTextManager.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
+using Utility;
 using Utility.SingleTon;
 
 namespace UI
 {
     public class FloatingTextManager : SingleMono<FloatingTextManager>
     {
+        [SerializeField] private ObjectPool<FieldFloatingText> pool;
         [SerializeField] private new Camera camera;
-        [SerializeField] private FieldFloatingText prefab;
 
         public void Display(Vector3 pos, string text)
         {
-            var fText = Instantiate(prefab, pos, camera.transform.rotation);
-            fText.Display(text);
+            var fText = pool.Get();
+            fText.transform.position = pos;
+            fText.transform.rotation = camera.transform.rotation;
+            fText.Display(text, fT => pool.Return(fT));
         }
     }
 }   
