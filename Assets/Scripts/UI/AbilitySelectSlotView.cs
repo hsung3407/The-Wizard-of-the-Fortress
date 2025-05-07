c0aa6537355da15529081de6991e1d2bfba05087
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-07 16:04:43 +0900

    update: ability select view
    UI 프리팹 추가

--- Assets/Scripts/UI/AbilitySelectSlotView.cs
+++ Assets/Scripts/UI/AbilitySelectSlotView.cs
@@ -1,20 +1,29 @@
 using System;
 using SO.Ability;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace UI
 {
     public class AbilitySelectSlotView : MonoBehaviour
     {
-        [SerializeField] private Button button;
-        
+        [SerializeField] private TextMeshProUGUI title;
+        [SerializeField] private Image icon;
+        [SerializeField] private TextMeshProUGUI description;
+        [SerializeField] private Button selectButton;
+
         public void Display(AbilityDataSO abilityData, Action<AbilityDataSO> callback)
         {
-            button.onClick.RemoveAllListeners();
-            button.onClick.AddListener(() => callback(abilityData));
+            if(!abilityData) return;
             
+            title.text = abilityData.AbilityName;
+            icon.sprite = abilityData.Icon;
+            description.text = abilityData.AbilityDescription;
             
+            selectButton.onClick.RemoveAllListeners();
+            selectButton.onClick.AddListener(() => callback(abilityData));
         }
     }
 }
