36854b0dbaf717eb55e656ff0fc3fcb397b3c855
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-07 16:35:19 +0900

    fix: ability select view
    레이아웃 사용을 위한 부모 설정 문제 해결

--- Assets/Scripts/UI/AbilitySelectView.cs
+++ Assets/Scripts/UI/AbilitySelectView.cs
@@ -9,20 +9,21 @@ namespace UI
     public class AbilitySelectView : MonoBehaviour
     {
         [SerializeField] private AbilitySelectSlotView prefab;
+        [SerializeField] private Transform selectSlotParent;
         private readonly List<GameObject> slots = new List<GameObject>();
 
         private void Start()
-        {   
+        {
             gameObject.SetActive(false);
         }
 
         public void Display(List<Ability> abilities, Action<Ability> onSelected)
         {
             gameObject.SetActive(true);
-            
+
             foreach (var ability in abilities)
             {
-                var slot = Instantiate(prefab, transform);
+                var slot = Instantiate(prefab, selectSlotParent);
                 slots.Add(slot.gameObject);
                 slot.Display(ability, onSelected += _ => { Clear(); });
             }
