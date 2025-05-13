af467118a02165dbddd267cfd136934f54be5b7f
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-13 11:18:06 +0900

    Update IngameManager.cs
    웨이브 간격 조절

--- Assets/Scripts/Ingame/IngameManager.cs
+++ Assets/Scripts/Ingame/IngameManager.cs
@@ -35,7 +35,7 @@ namespace Ingame
 
         private IEnumerator IngameFlow()
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(4f);
             
             yield return StartCoroutine(AbilitySelectFlow());
             
