30d78631f588f3b57451bb57c9a349f0eec664d8
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-13 10:48:11 +0900

    Update Test.cs

--- Assets/Level/Scenes/Test.cs
+++ Assets/Level/Scenes/Test.cs
@@ -1,14 +1,34 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Level.Scenes
 {
     public class Test : MonoBehaviour
     {
-        private void Start()
+        Coroutine _coroutine;
+        
+        private void Update()
         {
-            Func<bool> action = null;
-            Debug.Log(!action?.Invoke() ?? false);
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                if (_coroutine != null)
+                {
+                    Debug.Log("Stop");
+                    StopCoroutine(_coroutine);
+                }
+                Debug.Log("Start");
+                _coroutine = StartCoroutine(TestC(_coroutine));
+            }
+        }
+
+        private IEnumerator TestC(Coroutine self)
+        {
+            Debug.Log("SC");
+            yield return null;
+            Debug.Log("EC");
+
+            self = null;
         }
     }
 }
