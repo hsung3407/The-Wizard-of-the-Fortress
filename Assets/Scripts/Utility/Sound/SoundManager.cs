00d5a0360d9fd011de1b2cc55309e4731298b4e9
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-13 21:13:40 +0900

    Update SoundManager.cs

--- Assets/Scripts/Utility/Sound/SoundManager.cs
+++ Assets/Scripts/Utility/Sound/SoundManager.cs
@@ -1,22 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Utility.SingleTon;
 
-namespace Utility
+namespace Utility.Sound
 {
+    public enum SFXType
+    {
+        MonsterHit,
+    }
+    
     [RequireComponent(typeof(AudioSource))]
     public class SoundManager : SingleMono<SoundManager>
     {
         private AudioSource _audioSource;
+        private Dictionary<SFXType, AudioClip> _sfxClips = new();
 
         protected override void Awake()
         {
             base.Awake();
             _audioSource = GetComponent<AudioSource>();
+            var data = Resources.LoadAll<SFXDataSO>("SFXDataSO");
+            foreach (var sfxData in data)
+            {
+                _sfxClips.Add(sfxData.Type, sfxData.Clip);
+            }
+            
+            canBeDestroy = false;
         }
 
         public void PlaySFX(AudioClip source)
         {
             _audioSource.PlayOneShot(source);
+        }
+        
+        public void PlaySFX(SFXType type)
+        {
+            _audioSource.PlayOneShot(_audioSource.clip = _sfxClips[type]);
         }
     }
 }
