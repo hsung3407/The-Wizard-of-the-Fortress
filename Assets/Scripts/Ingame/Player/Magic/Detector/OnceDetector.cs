49e7342302653287aa41b482fa816e2b6365ffe0
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-14 16:42:00 +0900

    update: once detector

--- Assets/Scripts/Ingame/Player/Magic/Detector/OnceDetector.cs
+++ Assets/Scripts/Ingame/Player/Magic/Detector/OnceDetector.cs
@@ -1,15 +1,28 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Ingame.Player.Magic.Detector
 {
     public class OnceDetector : DetectorBase
     {
+        private Coroutine _coroutine;
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Enemy")) return;
             var enemy = other.GetComponent<Enemy>();
             Detect(enemy);
+
+            if (_coroutine == null) _coroutine = StartCoroutine(Wait());
+        }
+
+        private IEnumerator Wait()
+        {
+            yield return null;
+            GetComponent<Collider>()
+                .enabled = false;
         }
     }
 }
