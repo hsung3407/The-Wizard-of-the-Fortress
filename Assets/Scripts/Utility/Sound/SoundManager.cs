ac3de0d9b84b251941ae3fcf8f1ae9203512e3ab
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-15 23:37:12 +0900

    update: sound manager
    음악 재생 기능 추가

--- Assets/Scripts/Utility/Sound/SoundManager.cs
+++ Assets/Scripts/Utility/Sound/SoundManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Utility.SingleTon;
@@ -16,11 +17,15 @@ namespace Utility.Sound
         private AudioSource _audioSource;
         private Dictionary<SFXType, AudioClip> _sfxClips = new();
         private Dictionary<SFXType, float> _playedTime = new();
+        
+        private Coroutine _musicChangeCoroutine;
 
         protected override void Awake()
         {
             base.Awake();
             _audioSource = GetComponent<AudioSource>();
+            _audioSource.loop = true;
+            
             var data = Resources.LoadAll<SFXDataSO>("SFXDataSO");
             foreach (var sfxData in data)
             {
@@ -41,6 +46,43 @@ namespace Utility.Sound
             if(_playedTime[type] >= Time.time) return;
             _playedTime[type] = Time.time;
             _audioSource.PlayOneShot(_sfxClips[type], volume);
+        }
+
+        public void PlayMusic(AudioClip clip, float volume)
+        {
+            if(_musicChangeCoroutine != null) StopCoroutine(_musicChangeCoroutine);
+            _musicChangeCoroutine = StartCoroutine(MusicChange(clip, volume));
+        }
+
+        private IEnumerator MusicChange(AudioClip clip, float volume)
+        {
+            float changTime = 1f;
+            
+            if(_audioSource.clip && _audioSource.isPlaying) yield return FadeOut(changTime / 2, volume);
+            _audioSource.clip = clip;
+            yield return FadeIn(changTime / 2, volume);
+
+            _musicChangeCoroutine = null;
+        }
+
+        private IEnumerator FadeOut(float time, float maxVolume)
+        {
+            for (float i = 0; i < time; i+= Time.deltaTime)
+            {
+                _audioSource.volume = maxVolume * (1 - i / time);
+                yield return null;
+            }
+            _audioSource.Stop();
+        }
+        
+        private IEnumerator FadeIn(float time, float maxVolume)
+        {
+            _audioSource.Play();
+            for (float i = 0; i < time; i+= Time.deltaTime)
+            {
+                _audioSource.volume = maxVolume * i / time;
+                yield return null;
+            }
         }
     }
 }
