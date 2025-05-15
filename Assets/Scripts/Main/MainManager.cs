6acce06d2ef3138d98d82d989b4c37da7d3a539d
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-15 23:39:38 +0900

    update: main manager
    음악 추가

--- Assets/Scripts/Main/MainManager.cs
+++ Assets/Scripts/Main/MainManager.cs
@@ -1,8 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Utility.Sound;
 
 public class MainManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip mainMusic;
+    
+    private void Start()
+    {
+        SoundManager.Instance.PlayMusic(mainMusic, 0.5f);
+    }
+
     public void StartStage()
     {
         SceneManager.LoadScene("Ingame");
