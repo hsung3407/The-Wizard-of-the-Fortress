c33476c37c9ff0249042e0fbd10cf68bcc65892c
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-20 15:00:12 +0900

    add: user info
    사용자의 데이터 관리를 위한 임시 클래스 추가

---
+++ Assets/Scripts/Main/UserInfo.cs
@@ -0,0 +1,9 @@
+namespace Main
+{
+    public static class UserInfo
+    {
+        public static string Name = "Player";
+        public static int Level = 1;
+        public static int MaxStage = 0;
+    }
+}
